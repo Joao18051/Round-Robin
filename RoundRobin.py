03370971b76f3c86e02afd5730837e66a73def3f
Joao18051 <joao.magalhaes.s1805@gmail.com>
2024-08-05 15:53:58 -0300

    Mudança na vasão

--- RoundRobin.py
+++ RoundRobin.py
@@ -14,6 +14,9 @@ class Main:
         self.quantum = quantum /1000000 #Microsegundos
         self.amountProcess = amountProcess
         self.processes = [0 for _ in range(amountProcess)]
+        self.awaitTime = 0
+        self.returnTime = 0
+        self.vasao = 0
     
     def start(self):
         #Inicia os processos
@@ -21,19 +24,24 @@ class Main:
             self.processes[i] = Process()
 
         completedProcesses = []
+        vasao = 0
         startTime = time.time()
 
         while len(self.processes) > 0:
             currentWait = time.time()
             current = self.processes.pop(0)
-
+            #print(f"Processo atual: Processo {current.burstTime}") Linha comentada
             executionTime = min(self.quantum, current.remainingTime)
             current.remainingTime -= executionTime
             current.returnTime += executionTime
 
             if current.remainingTime == 0:
                 completedProcesses.append(current)
-            
+
+                endTime = time.time()
+                if (endTime - startTime) * 1000000 > 1000: 
+                    self.vasao += 1
+                
             if current.remainingTime > 0:
                 self.processes.append(current)
 
@@ -50,20 +58,24 @@ class Main:
             averageWaitTime += completedProcesses[i].awaitTime
             averageReturnTime += completedProcesses[i].awaitTime + completedProcesses[i].returnTime
 
-        averageWaitTime = (averageWaitTime / self.amountProcess) * 1000
-        averageReturnTime = (averageReturnTime / self.amountProcess) * 1000
-        #Calcula o tempo médio de execução por processo
-        averageProcessTime = totalTime / self.amountProcess
-        #Estima a quantidade de processos por milisegundo
-        processesPerSecond = 1 / (averageProcessTime / 1000)
+        self.awaitTime = averageWaitTime = (averageWaitTime / self.amountProcess) * 1000
+        self.returnTime = averageReturnTime = (averageReturnTime / self.amountProcess) * 1000
 
-        print(f"Tempo médio de espera: {averageWaitTime:.2f} milisegundos")
-        print(f"Tempo médio de retorno: {averageReturnTime:.2f} milisegundos")
-        print(f"Processos por milisegundo: {processesPerSecond:.2f}")
-        
+quantum = 10000 #Microsegundos
+amountProcess = 100
 
-quantum = 10 #Microsegundos
-amountProcess = 100 
+m = 0
+espera = 0
+retorno = 0
+vasao = 0
+while m < 100:
+    main = Main(quantum, amountProcess)
+    main.start()
+    m += 1
+    espera += main.awaitTime
+    retorno += main.returnTime
+    vasao += main.vasao
 
-main = Main(quantum, amountProcess)
-main.start()
+print(f"Tempo médio de espera: {espera/100:.2f} milisegundos")
+print(f"Tempo médio de retorno: {retorno/100:.2f} milisegundos")
+print(f"Processos por milisegundos: {vasao/100:.2f}")
